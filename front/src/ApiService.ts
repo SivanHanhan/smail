e9558b405b41502563f49179ec679203bba6b488
Ronen Metelitsa <ron.metelitsa@gmail.com>
2024-06-30 08:44:36 +0300

    feat(front) add route to get all mails of specific user, fix log in route

--- front/src/ApiService.ts
+++ front/src/ApiService.ts
@@ -14,6 +14,12 @@ export const getMails = async () => {
   });
 };
 
+export const getMailsOfUser = async (user) => {
+  return await api().then(async (api) => {
+    return api.get(`/mail/user/${user.email}`);
+  });
+};
+
 export const signUp = async (user) => {
   return await api().then(async (api) => {
     return api.post(`/user/signUp`, user);
@@ -22,37 +28,6 @@ export const signUp = async (user) => {
 
 export const logIn = async (user) => {
   return await api().then(async (api) => {
-    return api.get(`/user/logIn`, user);
+    return api.post(`/user/logIn`, user);
   });
 };
-
-// export const updateCurrentPage = async (id, current_page) => {
-//   return await api().then(async (api) => {
-//     return api.put(`/books/currentPage`, {id, current_page});
-//   });
-// };
-
-// export const updateReadingStatus = async (id, reading_status) => {
-//   return await api().then(async (api) => {
-//     // return api.put(`/books/readingStatus`,reading_status);
-//     return api.put(`/books/readingStatus`, { id, reading_status});
-//   });
-// };
-
-export const createBook = async (book) => {
-  return await api().then(async (api) => {
-    return api.post(`/books`, book);
-  });
-};
-
-// export const deleteBook = async (id) => {
-//   return await api().then(async (api) => {
-//     return api.delete(`/books/${id}`);
-//   });
-// };
-
-// export const getAllreadingStatuses = async () => {
-//   return await api().then(async (api) => {
-//     return api.get('/readingStatuses');
-//   });
-// };
